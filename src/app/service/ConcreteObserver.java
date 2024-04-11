f9b4d526833b0a3207086b7a36ea9ba67e2fecc1
Jeka <jenyabikovec@gmail.com>
2024-04-11 21:12:17 +0300

    Fixing commit

--- src/app/service/ConcreteObserver.java
+++ src/app/service/ConcreteObserver.java
@@ -5,7 +5,7 @@ import app.model.Observer;
 public class ConcreteObserver implements Observer {
     @Override
     public void update(int temperature, String color, int weight) {
-        System.out.println("Color: " + color + ", Weight: " + weight + "g");
+        System.out.println("Color: " + color + ", Weight: " + weight + "g" + ", Temperature: " + temperature);
     }
 }
 
